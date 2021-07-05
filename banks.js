5fceb5f53d23ccee4872569a3c89b48e2cae2e3d
darren <darren__c@hotmail.com>
2021-07-05 11:49:06 +0900

    added img and class

--- banks.js
+++ banks.js
@@ -1,5 +1,12 @@
 class Bank {
-  constructor(bankName, location) {}
+  constructor(bankName, location) {
+    this.bankName = bankName;
+    this.location = location;
+  }
+
+  interest() {
+    console.log(`${this.bankName} at ${this.location} charges 2% interest.`);
+  }
 }
 
 export { Bank };
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import { Bank } from './banks.js';
 
 console.log(Bank);
+
+const citiBank = new Bank('CitiBank', 'New Deli');
+citiBank.interest();
+
+console.log(citiBank);
