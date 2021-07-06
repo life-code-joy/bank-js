176d37225c55c6576c9e0f2db71baa5873de6729
darren <darren__c@hotmail.com>
2021-07-06 11:10:32 +0900

    must fix bug- btn is crazy

--- banks.js
+++ banks.js
@@ -1,11 +1,17 @@
 class Bank {
-  constructor(bankName, location) {
+  constructor(bankName, location, numOfEmployees, dateOpened) {
     this.bankName = bankName;
     this.location = location;
+    this.numOfEmployees = numOfEmployees;
+    this.dateOpened = dateOpened;
   }
 
   interest() {
     console.log(`${this.bankName} at ${this.location} charges 2% interest.`);
+  }
+
+  static bankHeadquartersMsg() {
+    console.log('Hey the meeting is tomorrow at 5:00');
   }
 }
 
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import { Bank } from './banks.js';
 
 console.log(Bank);
 
-const citiBank = new Bank('CitiBank', 'New Deli');
+const citiBank = new Bank('CitiBank', 'New Deli', 1200, 'May 2 2010');
 citiBank.interest();
 
+// bankHeadquartersMsg();
 console.log(citiBank);
